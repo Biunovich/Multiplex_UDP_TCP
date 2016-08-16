f9d8139d4e17d534811cd9468376f52fd571b7ef
biunovich <biunovich@gmail.com>
2016-08-16 14:06:43 +0700

    Multiprocces

--- multi_multiproc_UDP_server.c
+++ multi_multiproc_UDP_server.c
@@ -1,48 +1,78 @@
 #include "mylib.h"
 int sock;
-void client_process(struct sockaddr_in addr_other)
+void *MulPlex(void *arg)
 {
-	close(sock);
 	char buf[MSGLEN];
-	struct sockaddr_in addr_me;
-	int struct_len = sizeof(addr_me);
-	if ((sock = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP)) == -1)
+	struct sockaddr_in addr_other;
+	int struct_len = sizeof(addr_other);
+	int  *sock_this = (int*)arg ,epfd, numfd,i;
+	struct epoll_event ev ,events[CLIENT];
+	epfd = epoll_create(1);
+	ev.events = EPOLLIN;
+	ev.data.fd = *sock_this;
+	if (epoll_ctl(epfd,EPOLL_CTL_ADD, *sock_this,&ev) == -1)
+		perror("epoll_ctl");
+	while(1)
+	{
+		if ((numfd = epoll_wait(epfd,events,1,-1)) == -1)
+			perror("epoll_wait");
+		for (i=0;i<numfd;i++)
+		{
+			if (events[i].data.fd == *sock_this)
+			{
+				if (recvfrom(*sock_this,buf,MSGLEN,0,(struct sockaddr *)&addr_other,&struct_len) == -1)
+				{
+					perror("recvfrom");
+				}
+				if (sendto(*sock_this,buf,MSGLEN,0,(struct sockaddr *)&addr_other,struct_len) == -1)
+				{
+					perror("sendto");
+				}
+			}
+		}
+	}
+}
+void client_process()
+{
+	pid_t pid;
+	pthread_t thread;
+	char buf[MSGLEN];
+	struct sockaddr_in addr_me ,addr_other;
+	int struct_len = sizeof(addr_other),i,sock_new;
+	if ((sock_new = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP)) == -1)
 	{
 		perror("socket");
 	}
 	addr_me.sin_family = AF_INET;
 	addr_me.sin_port = 0;
 	inet_aton(IPADDR,&addr_me.sin_addr);
-	if (bind(sock, (struct sockaddr *)&addr_me, sizeof(addr_me)) == -1)
+	if (bind(sock_new, (struct sockaddr *)&addr_me, sizeof(addr_me)) == -1)
 	{
 		perror("bind");
 	}
-	if (sendto(sock,"New port : ",MSGLEN,0,(struct sockaddr *)&addr_other,struct_len) == -1)
-	{
-		perror("sendto");
-	}
+	pthread_create(&thread,NULL,MulPlex,&sock_new);
 	printf("%d\n",htons(addr_other.sin_port));
-	while(1)
+	for (i=0;i<CLIENT;i++)
 	{
 		printf("Waiting data\n");
 		if (recvfrom(sock,buf,MSGLEN,0,(struct sockaddr *)&addr_other,&struct_len) == -1)
 		{
 			perror("recvfrom");
 		}
-		printf("%s\n", buf);
-		if (sendto(sock,buf,MSGLEN,0,(struct sockaddr *)&addr_other,struct_len) == -1)
+		if (sendto(sock_new,"Connection",MSGLEN,0,(struct sockaddr *)&addr_other,struct_len) == -1)
 		{
 			perror("sendto");
 		}
+		printf("%s\n", buf);
 	}
-	close(sock);
+	pid = fork();
+	if (pid == 0)
+		client_process();
 }
 void main()
 {
-	char buf[MSGLEN];
 	pid_t pid;
-	struct sockaddr_in addr_me ,addr_other;
-	int  struct_len = sizeof(addr_other),num=0;
+	struct sockaddr_in addr_me;
 	if ((sock = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP)) == -1)
 	{
 		perror("socket");
@@ -54,25 +84,11 @@ void main()
 	{
 		perror("bind");
 	}
-	while(1)
+	pid = fork();
+	if (pid == 0)
 	{
-		if (recvfrom(sock,buf,MSGLEN,0,(struct sockaddr *)&addr_other,&struct_len) == -1)
-		{
-			perror("recvfrom");
-		}
-		pid = fork();
-		if (pid == 0)
-		{
-			num=0;
-			while(num<100){
-			if (recvfrom(sock,buf,MSGLEN,0,(struct sockaddr *)&addr_other,&struct_len) == -1)
-			{
-				perror("recvfrom");
-			}
-			client_process(addr_other);
-			num++;
-			}
-		}
+		client_process();
 	}
+	wait(0);
 	close(sock);
 }
--- mylib.h
+++ mylib.h
@@ -8,11 +8,14 @@
 #include <sys/socket.h>
 #include <sys/time.h>
 #include <sys/select.h>
+#include <sys/epoll.h>
+#include <pthread.h>
 #include <error.h>
+#include <sys/wait.h>
 #define MSGLEN 512
 #define PORT 1337
 #define PORT1 1488
 #define IPADDR "127.0.0.1"
-#define CLIENT 16
+#define CLIENT 10
 #define BCASTIP "192.168.2.255"
 #define MCASTIP "239.0.0.11"
